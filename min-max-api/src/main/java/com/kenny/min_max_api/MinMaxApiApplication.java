9b8f57a1fdaa9e4f26539340d8ff7a4f49034691
okiyitooo <okiyitoo@gmail.com>
2025-05-14 18:02:07 -0400

    feat: Initialize React application with essential files and configurations
    - Added logo image (logo512.png) for the application.
    - Created manifest.json for PWA support with app metadata and icons.
    - Added robots.txt to manage web crawler access.
    - Implemented App.css for styling the main application component.
    - Developed App.js as the main component with a logo and link to React documentation.
    - Created App.test.js for basic testing of the App component.
    - Added index.css for global styles.
    - Set up index.js to render the App component and include performance reporting.
    - Included logo.svg for the application logo.
    - Implemented reportWebVitals.js for measuring performance metrics.
    - Set up testing environment with setupTests.js for Jest and Testing Library.

---
+++ min-max-api/src/main/java/com/kenny/min_max_api/MinMaxApiApplication.java
@@ -0,0 +1,13 @@
+package com.kenny.min_max_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinMaxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinMaxApiApplication.class, args);
+	}
+
+}
---
+++ min-max-api/src/test/java/com/kenny/min_max_api/MinMaxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenny.min_max_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinMaxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
